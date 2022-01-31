1c80fd07c40452c023b98ef570d47aa7c99a4599
Rodrigo A. Silva <rgbrasil2@gmail.com>
2022-01-31 14:00:13 -0300

    chore: Criando upload de arquivos

--- src/routes/category.routes.ts
+++ src/routes/category.routes.ts
@@ -1,8 +1,14 @@
 import { Router } from "express";
+import multer from "multer";
+
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
 import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 
 const categoryRouter = Router();
+
+const upload = multer({
+  dest: "./tmp",
+});
 
 categoryRouter.post("/", (request, response) => {
   return createCategoryController.handle(request, response);
@@ -10,6 +16,14 @@ categoryRouter.post("/", (request, response) => {
 
 categoryRouter.get("/", (request, response) => {
   return listCategoriesController.handle(request, response);
+});
+
+categoryRouter.post("/import", upload.single("file"), (request, response) => {
+  const { file } = request;
+
+  console.log(file);
+
+  return response.status(201).send();
 });
 
 export { categoryRouter };
