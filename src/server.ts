7d50edc28c760427385e41762534036e5da9046f
Rodrigo A. Silva <rgbrasil2@gmail.com>
2022-02-02 10:21:11 -0300

    chore: swagger docs

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import express from "express";
+import SwaggerUI from "swagger-ui-express";
 import { routes } from "./routes";
+import swaggerConfig from "./swagger.json";
 
 const app = express();
 
 app.use(express.json());
+app.use("/api-docs", SwaggerUI.serve, SwaggerUI.setup(swaggerConfig));
 app.use(routes);
 
 app.listen(3333, () => console.log("Server running on http://localhost:3333"));
